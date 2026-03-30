2a9942f3bc787f318d164f6d01f33af18cebb484
naadirlab <sharifnaadir@gmail.com>
2026-03-30 19:57:47 +0200

    chore: add S3 bucket with DynamoDB state locking

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+# Configure backend and state locking
+backend "s3" {
+    bucket         = "umami-terraform-state"   
+    key            = "terraform.tfstate"       
+    region         = "eu-central-1"           
+    dynamodb_table = "terraform-state-lock"   
+    encrypt        = true                      
+
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-central-1"
