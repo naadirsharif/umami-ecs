b2f20601bbdebf0ed6fc8a311a1797bab5f89ad4
naadirlab <sharifnaadir@gmail.com>
2026-03-31 22:10:13 +0200

    chore: improve VPC setup and variables

--- infra/main.tf
+++ infra/main.tf
@@ -2,57 +2,66 @@
 
 ## Create VPC
 resource "aws_vpc" "umami-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_vpc
   region = var.aws_region
+  tags = var.tags
 }
 
 ## Public Subnets
 resource "aws_subnet" "public-eu-central-1a" {
   vpc_id            = aws_vpc.umami-vpc.id
-  availability_zone = "eu-central-1a"
-  cidr_block        = "10.0.1.0/24"
+  availability_zone = var.availability_zones[0]
+  cidr_block        = var.cidrs_public_subnet[0]
+  tags              = var.tags
 }
 
 resource "aws_subnet" "public-eu-central-1b" {
   vpc_id            = aws_vpc.umami-vpc.id
-  availability_zone = "eu-central-1b"
-  cidr_block        = "10.0.2.0/24"
+  availability_zone = var.availability_zones[1]
+  cidr_block        = var.cidrs_public_subnet[1]
+  tags              = var.tags
 }
 
 resource "aws_subnet" "public-eu-central-1c" {
   vpc_id            = aws_vpc.umami-vpc.id
-  availability_zone = "eu-central-1c"
-  cidr_block        = "10.0.3.0/24"
+  availability_zone = var.availability_zones[1]
+  cidr_block        = var.cidrs_public_subnet[2]
+  tags              = var.tags
 }
 
 ## Private Subnets
 resource "aws_subnet" "private-eu-central-1a" {
   vpc_id            = aws_vpc.umami-vpc.id
-  availability_zone = "eu-central-1a"
-  cidr_block        = "10.0.4.0/24"
+  availability_zone = var.availability_zones[0]
+  cidr_block        = var.cidrs_private_subnet[0]
+  tags              = var.tags
 }
 
 resource "aws_subnet" "private-eu-central-1b" {
   vpc_id            = aws_vpc.umami-vpc.id
-  availability_zone = "eu-central-1b"
-  cidr_block        = "10.0.5.0/24"
+  availability_zone = var.availability_zones[1]
+  cidr_block        = var.cidrs_private_subnet[1]
+  tags              = var.tags
 }
 
 resource "aws_subnet" "private-eu-central-1c" {
   vpc_id            = aws_vpc.umami-vpc.id
-  availability_zone = "eu-central-1c"
-  cidr_block        = "10.0.6.0/24"
+  availability_zone = var.availability_zones[2]
+  cidr_block        = var.cidrs_private_subnet[2]
+  tags              = var.tags
 }
 
 ## IGW and NGW
 resource "aws_internet_gateway" "umami-igw" {
   vpc_id = aws_vpc.umami-vpc.id
   region = var.aws_region
+  tags   = var.tags
 }
 
 resource "aws_nat_gateway" "umami-nat" {
-  vpc_id = aws_vpc.umami-vpc.id
-  connectivity_type = "public"
+  vpc_id            = aws_vpc.umami-vpc.id
+  connectivity_type = var.nat_connectivity_type
+  tags              = var.tags
 }
 
 ## Public Route Table
@@ -63,6 +72,7 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.umami-igw.id
   }
+  tags = var.tags
 }
 
 ## Public Route Table Assocications
@@ -88,6 +98,7 @@ resource "aws_route_table" "private" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.umami-nat.id
   }
+  tags           = var.tags
 }
 
 ## Private Route Table Assocications
@@ -104,3 +115,9 @@ resource "aws_route_table_association" "private_eu_central_1c" {
   subnet_id      = aws_subnet.private-eu-central-1c.id
   route_table_id = aws_route_table.private.id
 }
+
+
+
+# ALB configs
+
+
