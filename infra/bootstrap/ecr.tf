e66cc63d1d85b647be9294c44cc7221d0c4d15cb
naadirlab <sharifnaadir@gmail.com>
2026-04-11 11:10:47 +0200

    feat: add container repository for app images

--- infra/bootstrap/ecr.tf
+++ infra/bootstrap/ecr.tf
@@ -1,7 +1,7 @@
 # ECR module: creates container repository for app image
 
 resource "aws_ecr_repository" "app_repo" {
-  name                 = var.ecr_name
+  name                 = var.ecr_repo_name
   image_tag_mutability = "IMMUTABLE"
 
   force_delete = false
