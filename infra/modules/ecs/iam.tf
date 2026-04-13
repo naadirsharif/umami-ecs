e51aff6e97e39c9a71a55f52066a1f9306e381f9
naadirlab <sharifnaadir@gmail.com>
2026-04-13 19:29:17 +0200

    fix: allow execution role to read SSM parameters

--- infra/modules/ecs/iam.tf
+++ infra/modules/ecs/iam.tf
@@ -19,3 +19,23 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy" "ecs_ssm_access" {
+  name = "ecs-ssm-access"
+  role = aws_iam_role.ecs_execution_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+          "ssm:GetParameters",
+          "ssm:GetParametersByPath"
+        ]
+        Resource = aws_ssm_parameter.db_connection_string.arn
+      }
+    ]
+  })
+}
