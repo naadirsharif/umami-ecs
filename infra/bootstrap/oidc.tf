f9f21483a31f9dde1052336c761a0ead45c4e44b
naadirlab <sharifnaadir@gmail.com>
2026-04-13 12:46:46 +0200

    fix: relax OIDC trust policy for GitHub Actions

--- infra/bootstrap/oidc.tf
+++ infra/bootstrap/oidc.tf
@@ -25,7 +25,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/*"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
           }
         }
       }
